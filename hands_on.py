7d5faa2733ef3908a6b402a6f0febdefb145fd46
Akheloes <fayssaloudbib@gmail.com>
2020-10-08 21:26:15 +0200

    max, min, mean and std of a column

--- hands_on.py
+++ hands_on.py
@@ -6,6 +6,17 @@ from local_setting import dataFilePath
 
 sales = pd.read_csv(dataFilePath, parse_dates=['Date'])
 
-heades = sales.head()
+# columnes_names = sales.columns.values
+# print(columnes_names)
 
-print(heades)
+# Columns of Customer_Age, exploring
+# print(sales['Customer_Age'].__class__)
+# print('number of customers : ', len(sales['Customer_Age']))
+
+# maximum, minimum and mean and standard deviation of customers age
+max_customers_age = sales['Customer_Age'].max()
+min_customers_age = sales['Customer_Age'].min()
+mean_customers_age = sales['Customer_Age'].mean()
+std_customers_age = sales['Customer_Age'].std()
+print(f'(max, min, mean, std) = ({max_customers_age}, {min_customers_age}, {mean_customers_age}, {std_customers_age})')
+
